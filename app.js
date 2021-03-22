a430535eb10c0d349ddd766374b19bdc7c680104
Matei Popescu <matei.popescu@hotmail.com>
2021-03-22 13:07:53 +0100

    tweak for capitalised letters

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const first = document.querySelector('#first_name')
 const last = document.querySelector('#last_name')
 const age = document.querySelector('#age')
+const gender = document.querySelector('#gender')
 const city = document.querySelector('#city')
 const save = document.querySelector('#save-button')
 const resultDiv = document.querySelector('.resultDiv')
@@ -8,8 +9,12 @@ const resultDiv = document.querySelector('.resultDiv')
 
 let results = []
 
+function capitaliseFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
 const displayRecord = () => {
-    if (!first.value || !last.value || !age.value || !city.value){
+    if (!first.value || !last.value || !age.value || !gender.value || !city.value){
         alert('Please fill all the boxes');
         return;
     } 
@@ -18,7 +23,7 @@ const displayRecord = () => {
     const newRecord = document.createElement('p')
     newRecord.classList.add('new-record')
     resultDiv.appendChild(newRecord)
-    newRecord.innerHTML = first.value + ' ' + last.value + ' ' + age.value + " " + city.value
+    newRecord.innerHTML = first.value + ' ' + last.value.toUpperCase() + ' ' + capitaliseFirstLetter(gender.value) + " " + age.value + " " + capitaliseFirstLetter(city.value)
 
     first.value = ''
     last.value = ''
